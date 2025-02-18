83ea3ab9acf75cb710f75854f2b7e45d270366b4
Federico Di Iorio <federico_metallica@hotmail.com>
2025-02-18 17:27:52 -0300

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
-import { useState } from "react";
-import { Button } from "./components";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-  const countMore = () => {
-    setCount((count) => count + 1);
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("http://localhost:8080/api/products");
+      if (!response.ok) {
+        throw new Error("Error al obtener los datos");
+      }
+      const jsonData = await response.json();
+
+      console.log(jsonData);
+      setData(jsonData);
+    } catch (error) {
+      setError(error);
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  return (
-    <>
-      <Button label={`Count is ${count}`} parentMethod={countMore} />
-    </>
-  );
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <h3>Cargando...</h3>;
+  }
+
+  if (error) {
+    return <h3>Error: {error.message}</h3>;
+  }
+
+  return <div>{JSON.stringify(data)}</div>;
 }
 
 export default App;

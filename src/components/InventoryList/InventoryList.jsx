16e52327e404020922472dc44538747d25478dd6
Federico Di Iorio <federico_metallica@hotmail.com>
2025-02-24 19:30:26 -0300

    fix: InventoryList

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -1,8 +1,20 @@
 import { getInventory } from "../../services";
 import { useAsync } from "../../hooks";
 import { ItemMapper } from "../../components";
+import { FlexContainerColumn } from "../../styled-components";
 
 export const InventoryList = () => {
-  const products = useAsync(getInventory);
+  const { data: products, loading, error } = useAsync(getInventory);
+
+  if (loading) return <h3>Cargando...</h3>;
+
+  if (error)
+    return (
+      <FlexContainerColumn>
+        <h3>Ups! Hubo un error :(</h3>
+        <p>{JSON.stringify(error)}</p>
+      </FlexContainerColumn>
+    );
+
   return <ItemMapper items={products} />;
 };
